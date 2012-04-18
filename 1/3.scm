bc55443dd3f9ca0846901d9ad182f857e2b41123
Kyle K <kyliek24@hotmail.co.uk>
2012-04-18 22:27:13 +0100

    added my own min implementation

--- 1/3.scm
+++ 1/3.scm
@@ -1,4 +1,13 @@
 
+(define (min . args)
+  (let loop ((smallest #f)
+             (accum args))
+    (cond
+      ((null? accum) smallest)
+      ((or (not smallest) (> (car accum) smallest))
+       (loop (car (accum) (cdr accum))))
+      (else (loop smallest (cdr accum))))))
+
 (define (sumsq a b c)
   (let ((n (min a b c)))
     (cond ((= a n) (+ (* b b) (* c c)))
